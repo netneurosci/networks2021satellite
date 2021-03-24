bd871deace3573a81cbe2bf094eddbd6782aa2c5
Anibal Solon <anibalsolon@gmail.com>
2021-03-24 10:01:07 -0400

    fix: return login to original state

--- js/init-firebase.js
+++ js/init-firebase.js
@@ -37,6 +37,7 @@ function signIn() {
 window.signIn = signIn;
 function signOut() {
   firebase.auth().signOut();
+  document.getElementById("loginStatus").innerHTML = `<a style="color:white" href="#" onclick="signIn()">Sign In</a>`;
 }
 window.signOut = signOut;
 function startFirebase() {
