3e7d718cbad12c210cce76fd589a62c6d99ade3c
r03ert0 <robertotoro@gmail.com>
2021-06-29 20:10:59 +0200

    update room occupancy

--- posterwall/index.js
+++ posterwall/index.js
@@ -8,6 +8,10 @@ const goToRoom = (roomName) => {
   location.href = `https://meet.jit.si/NetNeurosci2021_${roomName}`;
 }
 
+function openJit(id, name) {
+  window.open("room.html#"+id+"."+name);
+}
+
 const displayPosters = (posters) => {
   const z = 1; // 0.1;
   const g = 1000/(5221+6313) * 0.915;
@@ -21,14 +25,15 @@ const displayPosters = (posters) => {
     p.height *= g;
     //     d="M${p.x},${p.y}h${p.width}v${p.height}h${-p.width}v${-p.height}"
     // d="M${p.x},${p.y-10}h10v10h-10v-10"
-    svg += `<g transform="translate(${p.x},${p.y-13}) scale(0.2)">
-    <path d="M 31.848061,7.6290677 A 28.403238,22.039304 0 0 0 3.596657,29.668006 28.403238,22.039304 0 0 0 10.583386,44.14469 L 7.6648251,56.370967 20.270838,49.740068 A 28.403238,22.039304 0 0 0 32,51.707502 28.403238,22.039304 0 0 0 60.403343,29.668006 28.403238,22.039304 0 0 0 32,7.6290677 a 28.403238,22.039304 0 0 0 -0.151939,0 z"
-    fill="rgba(255,255,255,0.4)" onclick="goToRoom('${p.pid}');" style="cursor:pointer; pointer-events: visiblePainted"></path>
-    <path d="m 16.837732,20.440612 v 19.377113 h 24.589197 v -8.876977 l 3.412712,1.970421 5.397084,3.11609 v -6.232178 -6.232178 l -5.397084,3.116088 -3.412712,1.970423 v -8.208802 z"
-    fill="white" stroke-width="5" onclick="goToRoom('${p.pid}');" style="cursor:pointer; pointer-events: visiblePainted"></path>
+    const [id, name] = p.pid.split("_");
+    svg += `<g id="nn21-${id}" transform="translate(${p.x},${p.y-13}) scale(0.2)">
+      <path class="bubble" d="M 31.848061,7.6290677 A 28.403238,22.039304 0 0 0 3.596657,29.668006 28.403238,22.039304 0 0 0 10.583386,44.14469 L 7.6648251,56.370967 20.270838,49.740068 A 28.403238,22.039304 0 0 0 32,51.707502 28.403238,22.039304 0 0 0 60.403343,29.668006 28.403238,22.039304 0 0 0 32,7.6290677 a 28.403238,22.039304 0 0 0 -0.151939,0 z"
+      fill="rgba(255,255,255,0.4)" onclick="openJit('nn21-${id}', '${name}');" style="cursor:pointer; pointer-events: visiblePainted"></path>
+      <path class="video" d="m 16.837732,20.440612 v 19.377113 h 24.589197 v -8.876977 l 3.412712,1.970421 5.397084,3.11609 v -6.232178 -6.232178 l -5.397084,3.116088 -3.412712,1.970423 v -8.208802 z"
+      fill="white" stroke-width="5" onclick="openJit('nn21-${id}', '${name}');" style="cursor:pointer; pointer-events: visiblePainted"></path>
+      <text font-size="30" x="70" y="40" fill="white"></text>
     </g>
     `;
-    // svg += `<text x="${p.x+5}" y="${p.y+10}">${i} ${p.pid}</text>`
   }
   document.querySelector("#svg").innerHTML = svg;
 }
@@ -44,6 +49,60 @@ function updateOverlay() {
   const y = -(hsvg*display.height/display.width - 1000)/2 + 1000*(pan.y - 0.5);
   svg.setAttribute('viewBox', `${x} ${y} ${wsvg} ${hsvg}`);
   // document.querySelector("#svg-style").sheet.rules[0].style["transform"] = `scale(${1/zoom})`;
+}
+
+const updateVideochatIcon = (sel, count) => {
+  const el = document.querySelector(sel);
+  if(!el) {
+    return;
+  }
+
+  if (count === 0) {
+    el.querySelector("text").innerHTML = "";
+    el.querySelector(".bubble").setAttribute("fill", "rgba(255,255,255,0.4)");
+  } else {
+    el.querySelector("text").innerHTML = count;
+    el.querySelector(".bubble").setAttribute("fill", "rgb(0, 220, 0)");
+  }
+}
+
+function websocketDump(dump) {
+  for(key in dump) {
+    if(key !== "" &&  {}.hasOwnProperty.call(dump, key)) {
+      console.log("dump", key, dump[key]);
+      updateVideochatIcon(`#${key}`, parseInt(dump[key]));
+    }
+  }
+}
+
+function websocketUpdate(update) {
+  const {id: key, count} = update;
+
+  if (key.split("-")[0] !== "nn21") {
+    return;
+  }
+
+  console.log("update", key, count);
+  updateVideochatIcon(`#${key}`, parseInt(count));
+}
+
+function websocketOnMessage(e) {
+  let msg = JSON.parse(e.data);
+  if(msg.dump) {
+    websocketDump(msg.dump);
+  }
+  if(msg.update) {
+    websocketUpdate(msg.update);
+  }
+}
+
+function connectToWebsocket() {
+  wss = new ReconnectingWebSocket("wss://brainspell.org/vcrooms");
+  wss.onopen = () => {
+    console.log("send dump request");
+      wss.send(JSON.stringify({action: "dump"}));
+  }
+  wss.onmessage = websocketOnMessage;
 }
 
 let viewer;
@@ -69,6 +128,8 @@ const main = async () => {
 
   viewer.addHandler('animation', () => {updateOverlay();});
   viewer.addHandler('pan', () => {updateOverlay();});
+
+  connectToWebsocket();
 }
 
 main();
